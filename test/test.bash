7ee35867ecd5c2a655df335c9f170ce76f7ca833
yama <s24c1031tu@s.chibakoudai.jp>
2026-01-08 23:55:52 +0900

    Add functionality test script

---
+++ test/test.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2025 Yamato Okada
+# SPDX-License-Identifier: BSD-3-Clause
+
+dir=~
+[ "$1" != "" ] && dir="$1"
+
+cd $dir/ros2_ws
+colcon build
+source install/setup.bash
+
+ros2 launch fps_monitor fps_launch.py > /dev/null &
+PID=$!
+sleep 5
+
+res=$(timeout 10 ros2 topic echo /current_fps --once)
+kill $PID
+
+echo "$res" | grep "data" && exit 0
+exit 1
