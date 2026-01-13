479310e01c802cfa3f2ba1751f73d42c779d9761
yama <s24c1031tu@s.chibakoudai.jp>
2026-01-13 21:23:27 +0900

    Fix test bash

--- test/test.bash
+++ test/test.bash
@@ -9,7 +9,6 @@ ng () {
 
 res=0
 
-# Pythonの出力を即座に表示させるおまじない（重要！）
 export PYTHONUNBUFFERED=1
 
 source /opt/ros/humble/setup.bash
@@ -17,14 +16,11 @@ source /opt/ros/humble/setup.bash
 colcon build --packages-select robosys2025_fps_monitor || ng "$LINENO"
 source install/setup.bash
 
-# ノードを起動（10秒後に強制終了）
 timeout 10 ros2 launch robosys2025_fps_monitor fps_launch.py > /tmp/robosys2025_fps_monitor.log 2>&1 || true
 
-# 【デバッグ用】ログの中身を画面に出す（これで何が記録されたかGitHubで見れるようになります）
 cat /tmp/robosys2025_fps_monitor.log
 
-# "FPS" という文字が含まれているかチェック
-count=$(grep -c "FPS" /tmp/robosys2025_fps_monitor.log)
+count=$(grep -c "Hz" /tmp/robosys2025_fps_monitor.log)
 [ "$count" -ge 1 ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
